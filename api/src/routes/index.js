e256f00bb4420caa333bf033829c49afa1029800
Diego Zamora <diegoz.estudio@gmail.com>
2022-02-21 10:18:46 -0300

    db done

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,28 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const axios = require("axios");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+const getApiInfo = async () => {
+  const apiUrl = await axios.get("https://breakingbadapi.com/api/characters");
+  const apiInfo = await apiUrl.data.map((el) => {
+    return {
+      name: el.name,
+      img: el.img,
+      nickname: el.nickname,
+      status: el.status,
+      id: el.char_id,
+      occupation: el.occupation.map((el) => el),
+      birthday: el.birthday,
+      appearance: el.appearance.map((el) => el),
+      //appearance: [...el.appearance],
+    };
+  });
+};
 
 module.exports = router;
